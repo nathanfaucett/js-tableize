81d483c3a39706f6cb1bcd6740bda9fd6335a2bd
nathanfaucett <nathanfaucett@gmail.com>
2015-10-22 22:35:41 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,19 @@
+var tape = require("tape"),
+    inflections = require("inflections"),
+    tableize = require("..");
+
+
+tape("tableize(string : String[, camelcase : Boolean[, locale : String]])", function(assert) {
+    var en = inflections("en");
+
+    en.plural(/$/, "s");
+    en.plural(/(ch|sh|ss|[sxz])$/i, "$1es");
+    en.plural(/([^aeiouy])y$/i, "$1ies");
+
+    assert.equal(tableize("Box"), "boxes");
+    assert.equal(tableize("Sky"), "skies");
+    assert.equal(tableize("BikeRider", false), "bike_riders");
+    assert.equal(tableize("MegaBigSale"), "megaBigSales");
+
+    assert.end();
+});
